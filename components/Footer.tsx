fe771b13768e55c5eb0791c389bc15d8ca494202
Woen-Sug Choi <woensug.choi@gmail.com>
2023-01-30 14:49:30 +0900

    fix footnote link

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -35,7 +35,7 @@ export const FooterImpl: React.FC = () => {
 
   return (
     <footer className={styles.footer}>
-      <div className={styles.copyright}>Copyright 2023 <a href="https://woensug-choi.github.io"/>Woen-Sug Choi</a></div>
+      <div className={styles.copyright}>Copyright 2023 <a href="https://woensug-choi.github.io">Woen-Sug Choi</a></div>
 
       <div className={styles.settings}>
         {hasMounted && (
